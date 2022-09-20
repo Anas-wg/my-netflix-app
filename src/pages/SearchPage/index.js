5dc866a475a3217895030cf196b595ebc0ff21d9
CHO WangGi <70210457+Anas-wg@users.noreply.github.com>
2022-09-20 23:50:34 +0900

    feat: add searchPage Docs: add pages

---
+++ src/pages/SearchPage/index.js
@@ -0,0 +1,36 @@
+import axios from '../../api/axios';
+import React, { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+export default function SearchPage() {
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  }
+
+  let query = useQuery();
+  const searchTerm = query.get('q')
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    if (searchTerm) {
+      fetchSearchMovie(searchTerm);
+    }
+  }, [searchTerm]);
+
+  const fetchSearchMovie = async (searchTerm) => {
+    try{
+      const request = await axios.get(
+        `/search/multi?include_adult=false&query=${searchTerm}`
+      );
+      setSearchResults(request.data.results)
+    } catch(error) {
+      console.log("error", error)
+    }
+  };
+  
+
+
+  return (
+    <div>index</div>
+  )
+}
+
